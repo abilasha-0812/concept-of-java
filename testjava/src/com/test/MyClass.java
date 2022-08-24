d6e9122b93879bb95cdd676ac3813d9534f672e8
Abilasha <abilasha08121998@gmail.com>
2022-08-24 23:00:13 +0530

    all concepts of java are added

---
+++ testjava/src/com/test/MyClass.java
@@ -0,0 +1,21 @@
+package com.test;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		//Variable declaration
+		int firstNumber=30, secondNumber=30;//Camelcase variable naming
+		String name="Edubridge Learning";
+		float height=200.67f;
+		
+		
+		//output statements
+		System.out.println(firstNumber+secondNumber);
+		System.out.println(name);
+		System.out.println(height);
+		
+
+	}
+
+}
+
---
+++ testjava/src/com/test/Testjava.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class Testjava {
+	public static void main(String[] args) {
+		System.out.println("Welcome to java");
+	}
+}
+		
+	
+
+
